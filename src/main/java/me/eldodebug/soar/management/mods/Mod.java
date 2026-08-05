7dcfc26a7411496f0d1bb25b10ad66e69fc64b54
ratnanoda <kurudedayo@gmail.com>
2026-08-06 03:00:15 +0900

    Make shared Minecraft reference immutable

--- src/main/java/me/eldodebug/soar/management/mods/Mod.java
+++ src/main/java/me/eldodebug/soar/management/mods/Mod.java
@@ -11,7 +11,7 @@ import org.lwjgl.input.Keyboard;
 
 public class Mod {
 
-    public static Minecraft mc = Minecraft.getMinecraft();
+    public static final Minecraft mc = Minecraft.getMinecraft();
     public FontRenderer fr = mc.fontRendererObj;
 
     private TranslateText nameTranslate, descriptionTranslate;
