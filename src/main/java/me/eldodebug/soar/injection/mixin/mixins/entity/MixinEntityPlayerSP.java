266314c66659503bce1c8cdd72b40019303342f4
ratnanoda <kurudedayo@gmail.com>
2026-08-06 02:54:36 +0900

    Keep local player model on Scaffold server rotation

--- src/main/java/me/eldodebug/soar/injection/mixin/mixins/entity/MixinEntityPlayerSP.java
+++ src/main/java/me/eldodebug/soar/injection/mixin/mixins/entity/MixinEntityPlayerSP.java
@@ -1,7 +1,6 @@
 package me.eldodebug.soar.injection.mixin.mixins.entity;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -18,16 +17,14 @@ public class MixinEntityPlayerSP {
     private final EventUpdate glide$eventUpdate = new EventUpdate();
     private final EventMotionUpdate glide$eventMotionUpdate = new EventMotionUpdate();
 
-    @Unique
-    private boolean glide$scaffoldRotationSpoofed;
-    @Unique
-    private float glide$cameraYaw;
-    @Unique
-    private float glide$cameraPitch;
-
     @Inject(method = "onUpdate", at = @At("HEAD"))
     public void preOnUpdate(CallbackInfo ci) {
         glide$eventUpdate.call();
+    }
+
+    @Inject(method = "onUpdate", at = @At("RETURN"))
+    private void postOnUpdate(CallbackInfo ci) {
+        ScaffoldMod.applySilentRotationToPlayer();
     }
 
     @Inject(method = "sendChatMessage", at = @At("HEAD"), cancellable = true)
@@ -43,27 +40,6 @@ public class MixinEntityPlayerSP {
     @Inject(method = "onUpdateWalkingPlayer", at = @At("HEAD"))
     private void preOnUpdateWalkingPlayer(CallbackInfo ci) {
         glide$eventMotionUpdate.call();
-        glide$scaffoldRotationSpoofed = false;
-
-        if(ScaffoldMod.isSilentRotationActive()) {
-            EntityPlayerSP player = (EntityPlayerSP) (Object) this;
-            glide$cameraYaw = player.rotationYaw;
-            glide$cameraPitch = player.rotationPitch;
-            player.rotationYaw = ScaffoldMod.getSilentYaw();
-            player.rotationPitch = ScaffoldMod.getSilentPitch();
-            glide$scaffoldRotationSpoofed = true;
-        }
-    }
-
-    @Inject(method = "onUpdateWalkingPlayer", at = @At("RETURN"))
-    private void postOnUpdateWalkingPlayer(CallbackInfo ci) {
-        if(!glide$scaffoldRotationSpoofed) {
-            return;
-        }
-
-        EntityPlayerSP player = (EntityPlayerSP) (Object) this;
-        player.rotationYaw = glide$cameraYaw;
-        player.rotationPitch = glide$cameraPitch;
-        glide$scaffoldRotationSpoofed = false;
+        ScaffoldMod.applySilentRotationToPlayer();
     }
 }
