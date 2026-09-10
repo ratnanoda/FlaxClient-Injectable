970c37d941756ee01f7b9557c6eb2cdc3911352a
ratnanoda <kurudedayo@gmail.com>
2026-09-10 19:32:59 +0900

    Route FastPlace through Minecraft key input

--- src/main/java/me/eldodebug/soar/management/mods/impl/FastPlaceMod.java
+++ src/main/java/me/eldodebug/soar/management/mods/impl/FastPlaceMod.java
@@ -4,12 +4,12 @@ import java.util.Random;
 
 import org.lwjgl.input.Mouse;
 
-import me.eldodebug.soar.attach.MinecraftAccess;
 import me.eldodebug.soar.management.event.EventTarget;
 import me.eldodebug.soar.management.event.impl.EventUpdate;
 import me.eldodebug.soar.management.language.TranslateText;
 import me.eldodebug.soar.management.mods.Mod;
 import me.eldodebug.soar.management.mods.ModCategory;
+import me.eldodebug.soar.utils.mouse.MinecraftMouseInput;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 
@@ -17,6 +17,7 @@ public class FastPlaceMod extends Mod {
 
 	private final Random random = new Random();
 	private int targetDelayTicks = 1;
+	private int ticksUntilClick;
 	private long nextDelayRefreshAt;
 	private int streakUntilRefresh;
 
@@ -27,34 +28,49 @@ public class FastPlaceMod extends Mod {
 	@Override
 	public void onEnable() {
 		super.onEnable();
+		resetClickState();
 		refreshDelayTarget();
+	}
+
+	@Override
+	public void onDisable() {
+		super.onDisable();
+		resetClickState();
 	}
 
 	@EventTarget
 	public void onUpdate(EventUpdate event) {
-		
-		if(mc.thePlayer == null || mc.theWorld == null || !mc.inGameHasFocus || mc.currentScreen != null) {
-			return;
-		}
-		
-		if(!Mouse.isButtonDown(1)) {
+		if(!canFastPlace()) {
+			resetClickState();
 			return;
 		}
 
 		ItemStack heldItem = mc.thePlayer.getHeldItem();
-		
-		if(heldItem != null && heldItem.getItem() instanceof ItemBlock) {
-			long now = System.currentTimeMillis();
-			if(now >= nextDelayRefreshAt || streakUntilRefresh <= 0) {
-				refreshDelayTarget();
-			}
-
-			if(MinecraftAccess.getRightClickDelayTimer(mc) > targetDelayTicks) {
-				MinecraftAccess.setRightClickDelayTimer(mc, targetDelayTicks);
-			}
-
-			streakUntilRefresh--;
+		if(heldItem == null || !(heldItem.getItem() instanceof ItemBlock)) {
+			resetClickState();
+			return;
 		}
+
+		long now = System.currentTimeMillis();
+		if(now >= nextDelayRefreshAt || streakUntilRefresh <= 0) {
+			refreshDelayTarget();
+		}
+
+		if(ticksUntilClick <= 0) {
+			// Queue a real use-item press in Minecraft's input state instead of
+			// only shortening rightClickDelayTimer. The client then consumes this
+			// through the same KeyBinding press queue used by mouse input.
+			MinecraftMouseInput.click(mc.gameSettings.keyBindUseItem);
+			ticksUntilClick = targetDelayTicks;
+			streakUntilRefresh--;
+		} else {
+			ticksUntilClick--;
+		}
+	}
+
+	private boolean canFastPlace() {
+		return mc.thePlayer != null && mc.theWorld != null && mc.inGameHasFocus
+				&& mc.currentScreen == null && Mouse.isButtonDown(1);
 	}
 
 	private void refreshDelayTarget() {
@@ -69,5 +85,9 @@ public class FastPlaceMod extends Mod {
 
 		nextDelayRefreshAt = System.currentTimeMillis() + 55L + random.nextInt(125);
 		streakUntilRefresh = 4 + random.nextInt(9);
+	}
+
+	private void resetClickState() {
+		ticksUntilClick = 0;
 	}
 }
